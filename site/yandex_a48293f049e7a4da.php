fedb559958b54ac1b52ca0b60efb5500c5807de5
ZhanibekDD <kiyashev.zhanibek@mail.ru>
2026-09-14 18:35:16 +0500

    Serve exact Yandex verification response

---
+++ site/yandex_a48293f049e7a4da.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+header('Content-Type: text/html; charset=UTF-8');
+echo "<html>\n<head>\n    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n</head>\n<body>Verification: a48293f049e7a4da</body>\n</html>\n";
